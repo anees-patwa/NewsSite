9cb5d859e8f0e85a5ec6a8fb49165a6d3f05f4bf
Armando Q <qmando98@gmail.com>
2018-09-22 11:54:35 -0500

    Create login and non user pages and navbars

---
+++ nonUserNav.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">P&Q Blog</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item active">
+       
+      </li>
+    </ul>
+    <span class="navbar-item active">
+         <a class="nav-link" href="#">Login </a>
+    </span>
+  </div>
+</nav>
