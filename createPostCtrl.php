dd818d2dc56e2f0fe3b06ee4fb050e0c0ace5d20
apatwa17 <ahpandroid5@gmail.com>
2018-09-24 10:58:29 -0500

    post creation debug done

--- createPostCtrl.php
+++ createPostCtrl.php
@@ -2,17 +2,19 @@
 session_start();
 if(!isset($_SESSION['userID'])){
 	header("Location: home.php");
+	
 	exit();
 }
 
 $userID = $_SESSION['userID'];
 $title = $_POST['title'];
 $content = $_POST['content'];
-
+$link = "";
 require("dataBaseAnees.php");
 
 $insertNewStory = $mysqli->prepare("insert into stories (user_id, title, link, content) values (?, ?, ?, ?)");
 if(!$insertNewStory){
+	
 	printf("Query Prep Failed: %s\n", $mysqli->error);
 	exit();
 }
@@ -25,23 +27,26 @@ $insertNewStory->close();
 
 $getStoryID = $mysqli->prepare("select story_id from stories where title=?");
 if(!$getStoryID){
+	
 	printf("Query Prep Failed: %s\n", $mysqli->error);
 	exit();
 }
 $getStoryID->bind_param('s', $title);
 $getStoryID->execute();
 $getStoryID->bind_result($storyID);
+
 $getStoryID->fetch();
 $getStoryID->close();
+$storyID = (int) $storyID;
+$link = sprintf("http://ec2-18-223-143-71.us-east-2.compute.amazonaws.com/~apatwa/module3Grp/storyView.php?story_id=%i", $storyID);
 
-$link = sprintf("http://ec2-18-223-143-71.us-east-2.compute.amazonaws.com/~apatwa/module3Grp/storyView.php?story_id=%s", $storyID);
-
-$updateLink = $mysqli->prepare("update stories set link=? where title=?");
+$updateLink = $mysqli->prepare("update stories set link=? where story_id=?");
 if(!$updateLink){
+	
 	printf("Query Prep Failed: %s\n", $mysqli->error);
 	exit();
 }
-$updateLink->bind_param('ss', $link, $storyID);
+$updateLink->bind_param('si', $link, $storyID);
 $updateLink->execute();
 $updateLink->close();
 
