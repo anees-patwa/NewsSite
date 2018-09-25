c9a236a35854f7ce37c079dc637075d0ca9e47bf
apatwa17 <ahpandroid5@gmail.com>
2018-09-25 00:20:42 -0500

    story management done

---
+++ editStory.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+    if(isset($_SESSION['userID'])){
+      require('userNav.php');
+    } else {
+      header("Location: home.php");
+      exit();
+    }
+
+    $content = $_POST['content'];
+    $storyID = (int)$_POST['storyID'];
+
+    $userID = (int)$_SESSION['userID'];
+    require("dataBaseAnees.php");
+    $stmt = $mysqli->prepare("update stories set title=? and content=? where story_id=?");
+    if(!$stmt){
+        printf("Query Prep Failed: %s\n", $mysqli->error);
+        exit();
+    }
+    $stmt->bind_param('ssd', $title, $content, $storyID);
+    $stmt->execute();
+    exit();
+?>
--- storyAction.php
+++ storyAction.php
@@ -8,46 +8,49 @@
     }
     
     $action = $_POST['action'];
-    $commentID = (int)$_POST['commentID'];
+    $storyID = (int)$_POST['storyID'];
     if($action == "delete"){
         require("dataBaseAnees.php");
-        $deleteComment = $mysqli->prepare("delete from comments where comment_id=?");
+        $deleteComment = $mysqli->prepare("delete from stories where story_id=?");
         if(!$deleteComment){
             printf("Query Prep Failed: %s\n", $mysqli->error);
             exit();
         }
 
-        $deleteComment->bind_param('d', $commentID);
+        $deleteComment->bind_param('d', $storyID);
 
         $deleteComment->execute();
 
         $deleteComment->close();
 
         //redirect back to my comments page
-        header("Location: myCommentsView.php");
+        header("Location: myStoriesView.php");
         exit();
     } else if ($action == "edit"){
         require("dataBaseAnees.php");
-        $deleteComment = $mysqli->prepare("select content from comments where comment_id=?");
+        $deleteComment = $mysqli->prepare("select title, content from stories where story_id=?");
         if(!$deleteComment){
             printf("Query Prep Failed: %s\n", $mysqli->error);
             exit();
         }
 
-        $deleteComment->bind_param('d', $commentID);
+        $deleteComment->bind_param('d', $storyID);
 
         $deleteComment->execute();
-        $deleteComment->bind_param($content);
+        $deleteComment->bind_param($title, $content);
         $deleteComment->fetch();
         $deleteComment->close();
         printf("
-            <form action='editComment.php' method='post'>
+            <form action='editStory.php' method='post'>
+                <input type='text' name='title' placeholder='%s'>
                 <input type='text' name='content' placeholder='%s'>
-                <input type='hidden' name='commentID' value='%d'>
+                <input type='hidden' name='storyID' value='%d'>
                 <button type='submit'>Submit</button>
             </form>        
         ",
-        htmlentities($content), htmlentities($commentID)
+        htmlentities($title), 
+        htmlentities($content),
+        htmlentities($storyID)
     );
     exit();
     }
